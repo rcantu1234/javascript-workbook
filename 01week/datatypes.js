47952dd022f0bcad6581d3150e7b466f7f30e766
roelcantu <roelhcantu@gmail.com>
2018-04-19 19:24:24 -0500

    inital

--- 01week/datatypes.js
+++ 01week/datatypes.js
@@ -1,17 +1,16 @@
 // Create a function that returns the current date by using the new Date() object.
 function dateAndTime() {
   const day = "Today is : " + new Date();
-  
   return day;
 }
 console.log(dateAndTime());
 
-// Create a function (numberToString(num)) with argument that converts a number or integer to a string 
+// Create a function (numberToString(num)) with argument that converts a number or integer to a string
 // when using the toString() method.
 function numberToString(num) {
   const n1 = num.toString();
   const x = (typeof n1);
-  
+
   return "Number " + num +
       " has been converted to : " + x;
 }
@@ -23,16 +22,16 @@ console.log(numberToString(20));
 function stringToNumber(str) {
   const n2 = parseInt(str);
   const x = (typeof n2);
-  
+
   return "String " + str +
       " has been converted to : " + x;
 }
 console.log(stringToNumber("10"));
 
-// Create a function (typeOfFunction(value)) with a value as an argument and return its type. 
+// Create a function (typeOfFunction(value)) with a value as an argument and return its type.
 function typeOfFunction(value) {
   const x = (typeof value);
-  
+
   return "The value of " + value + " is a type of : " + x;
 }
 console.log(typeOfFunction(0));
@@ -44,7 +43,7 @@ function add2Numbers(num1, num2) {
 }
 console.log(add2Numbers(5, 5));
 
-// Create a function (trueFunction()) that evaluates to true when 
+// Create a function (trueFunction()) that evaluates to true when
 // values are divisible by two values (ex. 3 and 5) when using the && operand.
 function trueFunction(num1, num2) {
   let text = "";
@@ -58,8 +57,8 @@ function trueFunction(num1, num2) {
     "2 statements are true : " + text;
 }
   console.log(trueFunction(3, 5));
-  
-// Create a function (trueFunctionII(num1, num2)) that evaluates to true when 
+
+// Create a function (trueFunctionII(num1, num2)) that evaluates to true when
 // values are divisible by at least one of the two values (ex. 3 and 5) when using the || operand.
 function trueFunctionII() {
   let text = "";
@@ -72,11 +71,3 @@ function trueFunctionII() {
     "1 of 2 statements is true : " + text;
 }
 console.log(trueFunctionII());
-
-
-
-
-
-
-
-
