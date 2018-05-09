aba084d1d97277f782a38d204ec9b47bec39b26b
roelcantu <roelhcantu@gmail.com>
2018-05-09 12:33:35 -0500

    Made final changes

--- 04week/mastermind.js
+++ 04week/mastermind.js
@@ -1,5 +1,17 @@
 'use strict';
 
+// 1. User makes a guess => string.
+// 2. Check for length of string.  Invalid
+//    if string is less than 4 letters.
+// 3. Each letter needs to be in letters array.
+// 4. If valid, check if guess === solution.
+// 5. If it matches, notify the player that they won!!!
+// 6. Reset the board when player wins!!!
+// 7. If it doesn't match, give the player hints.
+// 8. Store guesses in array.
+// 9.  User has 10 guesses.  Game over when maxed guesses.
+// 10.  Reset game.
+
 const assert = require('assert');
 const readline = require('readline');
 const rl = readline.createInterface({
@@ -28,13 +40,57 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function generateHint() {
+function generateHint(guess) {
   // your code here
+  const guessArr = guess.split('');
+  const solutionArr = solution.split('');
+  let rightPlaceRightLetter = 0;
+  let wrongPlaceRightLetter = 0;
+
+  guessArr.forEach((letter, index) => {
+    if(solutionArr[index] == letter) {
+      console.log(letter, '1');
+      rightPlaceRightLetter += 1;
+    } else if(solutionArr.indexOf(letter) != -1) {
+      console.log(letter, '2');
+      wrongPlaceRightLetter += 1;
+    }
+  })
+  console.log(`${rightPlaceRightLetter} right place right letter
+     ${wrongPlaceRightLetter} wrong place right letter`);
 }
 
+const isValidGuess = (guess) => {
+  let allLettersAcceptable = true;
+  const guessArr = guess.split('');
+  guessArr.forEach((letter, index) => {
+    if(letters.indexOf(letter) == -1) {
+      allLettersAcceptable = false;
+    }
+  })
+  return guess.length === 4 && allLettersAcceptable;
+}
 function mastermind(guess) {
   solution = 'abcd'; // Comment this out to generate a random solution
   // your code here
+  if(isValidGuess(guess)) {
+    console.log('Keep going!!!');
+    if(solution == guess) {
+      console.log('You won!!!');
+      // reset the board
+      board = [];
+    } else {
+      generateHint(guess);
+      board.push(guess);
+      if(board.length < 10) {
+          console.log('you lose');
+          board = [];
+      }
+      console.log('Guess again');
+    }
+  } else {
+    console.log('Invalid guess');
+  }
 }
 
 
