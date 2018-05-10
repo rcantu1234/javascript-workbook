e0e124087596a6e110cc7c393d3796c74392544d
roelcantu <roelhcantu@gmail.com>
2018-05-10 06:51:17 -0500

    test

--- 04week/mastermind.js
+++ 04week/mastermind.js
@@ -7,9 +7,9 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-let board = [];
-let solution = '';
-let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+let board = []; //array
+let solution = 'abcd';
+const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
 
 function printBoard() {
   for (let i = 0; i < board.length; i++) {
@@ -28,19 +28,73 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function generateHint() {
+function generateHint(guess) {
   // your code here
+  const guessArr = guess.split('');
+  const solutionArr = solution.split('');
+  let rightPlaceRightLetter= 0;
+  let wrongPlaceRightLetter= 0;
+  guessArr.forEach((letter, index)=> {
+    //guess afcb 1
+    //solution abcd
+    if (solutionArr[index] == letter) {
+         console.log(letter, '1')
+        // rightPlaceRightLetter = rightPlaceRightLetter + 1
+        // rightPlaceRightLetter +=
+        rightPlaceRightLetter ++
+    }else if(solutionArr.indexOf(letter) != -1){
+        console.log(letter, '2')
+        wrongPlaceRightLetter ++
+    }
+  });
+
+  console.log(`${rightPlaceRightLetter} right place right letter,
+    ${wrongPlaceRightLetter} wrong place right letter`)
+  //2 right place, right letter, 1 right color wrong letter
+}
+
+const isValidGuess=(guess)=>{
+  const guessArr = guess.split('');
+
+  let allLettersAcceptable = true;
+  guessArr.forEach((letter, index)=> {
+    if (letters.indexOf(letter) == -1){
+      allLettersAcceptable = false;
+    }
+  });
+
+  return guess.length === 4 && allLettersAcceptable
 }
 
 function mastermind(guess) {
-  solution = 'abcd'; // Comment this out to generate a random solution
-  // your code here
+  // solution = 'abcd'; // uncomment this when developing
+  // your code here;
+
+  if (isValidGuess(guess)) {
+
+    if(solution == guess){
+      console.log('you won!');
+      //reset the board
+      board = [];
+    }else{
+      generateHint(guess);
+      board.push(guess);
+      if (board.length > 9 ){
+        console.log('you lose')
+        board = [];
+      }
+      console.log('not the solution, guess again')
+    }
+
+  }else {
+    console.log('invalid guess')
+  }
 }
 
 
 function getPrompt() {
   rl.question('guess: ', (guess) => {
-    mastermind(guess);
+    console.log( mastermind(guess) );
     printBoard();
     getPrompt();
   });
