7d8f35c22c52e0a063afbe78c86504070c656142
roelcantu <roelhcantu@gmail.com>
2018-05-12 17:47:33 -0500

    added for, while, and do while loops

--- 04week/loop.js
+++ 04week/loop.js
@@ -10,8 +10,27 @@ const persons = {
 for(const person in persons) {
     console.log(`${person}`);
 }
+console.log('\n');
 
-let i = 1;
-while(i <= 1000) {
-  console.log(i);
+// While loop to print 1000 numbers
+console.log('While Loop');
+let j = 1;
+while(j <= 1000) {
+  console.log(j);
+  j++;
 }
+console.log('\n');
+
+// For loop to print 1000 numbers
+console.log('For Loop');
+// for (let i = 1; i <= 1000; i++) {
+//   console.log(i);
+// }
+// console.log('\n');
+// do...while loop to print 1000 numbers
+// console.log('do...While Loop');
+// let k = 1;
+// do {
+//     console.log(k);
+//     k++;
+// } while(k <= 1000);
